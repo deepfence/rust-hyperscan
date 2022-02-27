deba97ba46edd6d73c344463e83cdc044afc6a4e
Alessandro Decina <alessandro.d@gmail.com>
2022-02-27 09:25:36 +0000

    Make Scratch Send + Sync

--- hyperscan/src/runtime/scratch.rs
+++ hyperscan/src/runtime/scratch.rs
@@ -10,7 +10,7 @@ use crate::ffi;
 
 foreign_type! {
     /// A large enough region of scratch space to support a given database.
-    pub unsafe type Scratch {
+    pub unsafe type Scratch: Send + Sync {
         type CType = ffi::hs_scratch_t;
 
         fn drop = free_scratch;
